eb90fbeeda2842839b3c5f2622079ecd1537d5f3
jay nam <woalskdl@gmail.com>
2023-12-10 23:09:08 +0900

    Header 영역 작성

--- App.js
+++ App.js
@@ -1,72 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Typography } from './src/components/Typography';
-import { LocalImage } from './src/components/LocalImage';
-import { RemoteImage } from './src/components/RemoteImage';
-import { Icon } from './src/components/Icons';
-import { Badge } from './src/components/Badge';
-import { Button } from './src/components/Button';
-import { Divider } from './src/components/Divider';
-import { Spacer } from './src/components/Spacer';
+import { StyleSheet, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Header } from './src/components/header/Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Typography color='red' fontSize={20}>
-        {/* 이것은 <Typography color='green' fontSize={20}>텍스트</Typography> 입니다. */}
-        이것은 텍스트 입니다.
-      </Typography>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Header>
+          <Header.Group>
+            <Header.Icon iconName='arrow-back' />
+            <Header.Title title={'HEADER'}/>
+          </Header.Group>
 
-      <Divider/>
-
-      <LocalImage 
-        localAsset={require('./assets/favicon.png')} 
-        width={50} 
-        height={50}
-      />
-
-      <Spacer space={20}/>
-
-      <RemoteImage 
-        url={'https://miro.medium.com/v2/resize:fit:1400/1*6GWNVdu_xV-OhgjYy4shcA.png'}
-        width={200}
-        height={100}
-      />
-
-      <Icon name='home' size={40} color='red'/>
-
-      <View style={{ flexDirection: 'row '}}>
-        <Badge fontSize={10}>
-          <Typography>BADGE</Typography>
-        </Badge>
-
-        <Badge fontSize={10}>
-          <Icon name='home' size={50} color='black' />
-        </Badge>
+          <Header.Icon iconName='close'/>
+        </Header>
       </View>
-
-      <Divider/>
-
-      <View style={{ flexDirection: 'row ', marginTop: 32}}>
-        <Button onPress={() => {console.log('PRESSED BUTTON')}}>
-          <Typography>BUTTON</Typography>
-        </Button>
-
-        <Button onPress={() => {console.log('PRESSED ICON BUTTON')}}>
-          <Icon name='home' size={50} color='green'/>
-        </Button>
-
-      </View>
-
-    </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/components/header/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Dimensions, View } from "react-native";
+import { SafeAreaInsetsContext } from 'react-native-safe-area-context';
+import { Spacer } from "../Spacer";
+import { HeaderTitle } from './HeaderTitle';
+import { HeaderIcon } from "./HeaderIcon";
+import { HeaderGroup } from './HeaderGroup';
+
+const { width } = Dimensions.get('window');
+
+export class Header extends React.Component {
+  render() {
+    return (
+      <SafeAreaInsetsContext.Consumer>
+        {insets => (
+          <View style={{paddingTop:insets.top, backgroundColor:'white'}}>
+            <View 
+              style={{ 
+                width: width, 
+                height:56, 
+                flexDirection:'row', 
+                borderBottomColor: 'gray', 
+                borderBottomWidth: 1,
+                alignItems:'center'
+              }}
+            >
+              <Spacer horizontal={true} space={12}/>
+              <View style={{ flex:1, flexDirection:'row', justifyContent:'space-between'}}>
+                {this.props.children}
+              </View>
+              <Spacer horizontal={true} space={12}/>
+            </View>
+          </View>
+        )}
+      </SafeAreaInsetsContext.Consumer>
+    )
+  }
+}
+
+Header.Title = HeaderTitle;
+Header.Icon = HeaderIcon;
+Header.Group = HeaderGroup;
